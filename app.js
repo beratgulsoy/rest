c7f1873ff8e5e897bbce9727ca95696f0316739c
beratgulsoy <beratgulsoyy@gmail.com>
2023-11-06 22:38:23 +0300

    starting

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const feedRoutes = require("./routes/feed");
+
+const bp = require("body-parser");
+
+const app = express();
+
+app.use("/feed", feedRoutes);
+
+app.listen(8080);
