184930e1ca54768380eafa0f4e120dbb7639c12d
beratgulsoy <beratgulsoyy@gmail.com>
2023-11-10 00:47:58 +0300

    error handling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -10,10 +11,18 @@ const cors = require("cors");
 const app = express();
 
 app.use(bp.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(cors());
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect(
@@ -23,4 +32,3 @@ mongoose
     app.listen(8080);
   })
   .catch((err) => console.log(err));
-
