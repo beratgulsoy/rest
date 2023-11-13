7f050b0ebfa93ab0f867e275f8d628457ef18a95
beratgulsoy <beratgulsoyy@gmail.com>
2023-11-14 02:38:13 +0300

    auth to all routes

--- routes/feed.js
+++ routes/feed.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/posts", isAuth, feedController.getPosts);
 router.post(
   "/post",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -15,9 +16,10 @@ router.post(
   feedController.createPost
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 router.put(
   "/post/:postId",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -25,6 +27,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
