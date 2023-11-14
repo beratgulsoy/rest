52d22ea766065cf9c17bb557f7c001f8f6b589ea
beratgulsoy <beratgulsoyy@gmail.com>
2023-11-14 22:17:18 +0300

    socket io connection

--- app.js
+++ app.js
@@ -56,6 +56,15 @@ mongoose
     "mongodb+srv://berat:berat@cluster0.2nxyobm.mongodb.net/messages?retryWrites=true&w=majority"
   )
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("socket.io")(server, {
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+      },
+    });
+    io.on("connection", (socket) => {
+      console.log("Client connected.");
+    });
   })
   .catch((err) => console.log(err));
