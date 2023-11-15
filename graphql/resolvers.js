d09ee4748555765ae33f476d05451136424766e0
beratgulsoy <beratgulsoyy@gmail.com>
2023-11-16 02:11:33 +0300

    create post with graphql without creator

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,9 @@
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
+
 const User = require("../models/user");
+const Post = require("../models/post");
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
@@ -57,5 +59,39 @@ module.exports = {
       { expiresIn: "1h" }
     );
     return { token: token, userId: user._id.toString() };
+  },
+  createPost: async function ({ postInput }, req) {
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: "Title is invalid." });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: "Content is invalid" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    const post = await new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl,
+    });
+    const createdPost = await post.save();
+    //Add post to user's posts
+    return {
+      ...createdPost._doc,
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toDateString(),
+      updatedAt: createdPost.updatedAt.toDateString(),
+    };
   },
 };
