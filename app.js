3918b02b44d37a25bf1e9dcfe38c9af0cb04ab23
beratgulsoy <beratgulsoyy@gmail.com>
2023-11-16 23:50:08 +0300

    add post with image

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
@@ -44,6 +45,21 @@ app.use(cors());
 
 app.use(auth);
 
+app.put("/post-image", (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error("Not authenticated.");
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file provided." });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: "File stored.", filePath: req.file.path });
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -79,3 +95,9 @@ mongoose
     app.listen(8080);
   })
   .catch((err) => console.log(err));
+
+const clearImage = (filePath) => {
+  console.log(filePath);
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
